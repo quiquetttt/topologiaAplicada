cf35a66dc7a054ba0310fbd45000ceaf3ad22b88
Enrique Barroso <enrique.barroso@neurored.com>
2024-10-22 10:56:54 +0200

    4structure repo

---
+++ topologiaAplicada/P3_homologiaSimp/homologiaSimp.py
@@ -0,0 +1,19 @@
+"""
+-Definir una función que calcule la forma normal de Smith de una matriz con coeficientes en Z2
+-Definir dentro de la clase Complejo simplicial métodos que permitan:
+    1.Calcular la matriz borde para cada dimensión.
+    2.Calcular los números de Betti.
+-Calcular los números de Betti de los siguientes complejos simpliciales:
+    1.El tetraedro.
+    2.El borde del tetraedro.
+    3.El toro con las dos triangulaciones vistas en clase.
+    4.El plano proyectivo.
+    5.La botella de Klein.
+    6.El anillo.
+    7.El sombrero del asno.
+    8.Del complejo simplicial de la transparencia 4 del documento Homología Simplicial II.
+    9.Del doble toro.
+    10.De algunos alfa complejos.
+-Crear una función que calcule los números de Betti b_0 y b_1 de un complejo simplicial contenido en el plano utilizando el algoritmo incremental. 
+-Calcular los números de Betti de algunos alfa complejos del plano utilizando el algoritmo incremental.
+"""
